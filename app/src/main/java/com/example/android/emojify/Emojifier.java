c76e4c181304335e62a46d3fdf489a1475c07657
varunvgnc <Travel@12345>
2018-06-21 15:58:48 +0530

    Completed TODOs

---
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -0,0 +1,12 @@
+package com.example.android.emojify;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+public class Emojifier {
+
+    public static void detectFaces(Context context, Bitmap bitmap)
+    {
+
+    }
+}
